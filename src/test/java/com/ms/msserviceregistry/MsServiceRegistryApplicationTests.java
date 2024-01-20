40867b978ec5c5ca3436105b8331b817c4a59c62
krithijoshi <krithijoshi@gmail.com>
2024-01-21 00:48:18 +0530

    First Commit

---
+++ src/test/java/com/ms/msserviceregistry/MsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.msserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
